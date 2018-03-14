0cb762136178b5f95bb478a5552f7a04e67094d4
alanHarper123 <1531508001@qq.com>
2018-03-14 22:06:08 +0800

    todo app

--- src/index.js
+++ src/index.js
@@ -6,16 +6,65 @@ const styles = {
   fontFamily: 'sans-serif',
   textAlign: 'center',
 };
-
+let id = 0
+const Todo = props => (
+  <li>
+    <input type = 'checkbox' checked = {props.todo.checked} onChange = {props.onToggle}/>
+    <button onClick = {props.onDelete}>Delete</button>
+    <span>{props.todo.text}</span>
+  </li>
+)
+  
 class App extends React.Component {
+  constructor(){
+    super()
+    this.state  = {
+      todos : [],
+    }
+  }
+
+  addTodo(){
+    const text = prompt("give me some todo text please!")
+    this.setState({
+      todos:[...this.state.todos,{id:id++,text:text,checked:false}],
+      })
+  }
+  removeTodo(id){
+    this.setState({
+      todos:this.state.todos.filter(todo => todo.id !== id)
+    })
+  }
+  toggleTodo(id){
+    this.setState({
+      todos:this.state.todos.map(todo => {
+        if(todo.id === id){
+          todo.checked = !todo.checked
+        }
+        return todo
+      })
+    })
+  }
+
   render(){
     return (<div style={styles}>
       <Hello name="CodeSandbox" />
-      <h2>{this.props.count}</h2>
+      <div>
+        <div>todo count: {this.state.todos.length}</div>
+        <div>unchecked todo count: {this.state.todos.filter(todo=>!todo.checked).length}</div>
+        <button onClick={this.addTodo.bind(this)}>AddTodo</button>
+        <ul>
+          {this.state.todos.map(todo => 
+            <Todo onDelete = {()=>this.removeTodo(todo.id)}
+                  onToggle = {()=>this.toggleTodo(todo.id)}
+             todo = {todo}/>
+            )}
+        </ul>
+        
+      </div>
     </div>
     )
   }
 }
   
-let count = 0
-setInterval(()=>render(<App count={count++}/>, document.getElementById('root')),1000)
+
+render(<App />, document.getElementById('root'))
